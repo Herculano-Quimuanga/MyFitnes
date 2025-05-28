5c4c4d5920a182b4cc456e307bcd0f978b28cd43
Herculano-Quimuanga <herculanoquimuanga0@gmail.com>
2025-05-28 14:44:58 +0100

    Create the components for app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./main/header/header.component";
+import { BannerComponent } from "./content/banner/banner.component";
+import { AboutComponent } from "./content/about/about.component";
+import { ProgramsComponent } from "./content/programs/programs.component";
+import { ServicesComponent } from "./content/services/services.component";
+import { ReviewComponent } from "./content/review/review.component";
+import { FooterComponent } from "./main/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    HeaderComponent,
+    BannerComponent,
+    AboutComponent,
+    ProgramsComponent,
+    ServicesComponent,
+    ReviewComponent,
+    FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
