b0b3eb1ff9ffb55867ed4896d6d913d2532a69b8
Herculano-Quimuanga <herculanoquimuanga0@gmail.com>
2025-06-08 13:03:54 +0100

    new deploy

--- dist/my-fitnes/server/angular-app-manifest.mjs
+++ dist/my-fitnes/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 19814, hash: 'd5e16f5ff88e9cc3f4b4664f2862843b06e29c2522620a22db316bdfed43bfe5', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1141, hash: 'e3d63abdc19054a020f9345eb4f3ab9a3656471b333da339dd2c7dce689e018a', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 34334, hash: 'd1566f15b6bb8cdc29cf911a34376d198f16477d5dd35b4997defa304e3a47f5', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-OWQU75A3.css': {size: 65033, hash: 'Z+EUFCu7xYY', text: () => import('./assets-chunks/styles-OWQU75A3_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 19814, hash: '04062a80d9121f97a51f6a15e19271d71f1775b0adcf6922ed3b4e2e2a24bf2c', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1141, hash: '4ad23f1bb67e73f52aab4a7d974841f1e39a8c51a77f52fd9fc217ea38769c92', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 37005, hash: '330d47294b150d8907a6bd5e130224b3b01101e9d09a5dbcd442bd464e2b2ab8', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-OFZ2AH74.css': {size: 65101, hash: 'VgZ2tU8nBZc', text: () => import('./assets-chunks/styles-OFZ2AH74_css.mjs').then(m => m.default)}
   },
 };
